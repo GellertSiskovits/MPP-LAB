98490a4f58eecec92fe5787a6e4750c5cfd40302
gellert.peter <geraldlionb@gmail.com>
2018-04-15 17:22:29 +0300

    [Server update - Server Side Complete] #requires fixes

---
+++ src/main/java/server/MyServerTest.java
@@ -0,0 +1,34 @@
+package server;
+
+import Ui.UiController;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MyServerTest extends Application{
+    Parent root;
+    Scene scene;
+    UiController uiController = new UiController();
+
+    public  void start(Stage stage){
+
+        //  Menu.run();
+        try {
+            root = FXMLLoader.load(getClass().getResource("/UiFX.fxml"));
+
+            scene = new Scene(root,600,400);
+            stage.setScene(scene);
+            MyServer myServer = new MyServer(this.uiController);
+
+            stage.show();
+            myServer.startRunning();
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+       // MyServer myServer = new MyServer(this.uiController);
+
+    }
+}
